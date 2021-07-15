d5603ea444d10b31ce93c8fdb508118ff6e7c28c
Andrew Johnson <andrew.r.johnson@postgrad.curtin.edu.au>
2021-07-15 16:26:00 +0800

    Update init_all_chroots.sh

--- init_all_chroots.sh
+++ init_all_chroots.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+mkdir -p ~/.stanc3 && cd ~/.stanc3
+git clone https://github.com/stan-dev/stanc3 && cd stanc3
+bash -x scripts/install_ocaml.sh
+bash -x scripts/install_build_deps.sh
+
 declare -a ArchArray=("arm64" "armel" "armhf" "i386"
                       "amd64" "mipsel" "mipsel64"
                       "ppc64el" "s390x")
